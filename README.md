501ac9f5e72aa4c359abbde7a984e3df1fa83d34
Daniel Campbell <Daniel_Campbell2@student.uml.edu>
2016-11-17 12:08:53 -0500

    You this versions accordian might suck

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Supplify
 Application that retrieves a concise and thorough background on any supplement.
 
+updataded ui -Dan
+Accordian still in progress for this version
+
 Here's the link to our project proposal on [Google Docs](https://docs.google.com/document/d/1M4Ny7-L1ZS2ZprWIovuIOqms2MEHW080MaYKJfZrReA/edit?usp=sharing).
---
+++ app/src/main/java/com/example/supplify2/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.supplify2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.action_delete_history:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
